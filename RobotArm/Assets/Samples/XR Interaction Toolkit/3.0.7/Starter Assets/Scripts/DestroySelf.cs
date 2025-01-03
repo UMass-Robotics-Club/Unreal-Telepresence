c84b292f26e23393213251810444de2f1777eae9
idealmonke <ty913913@yahoo.com>
2025-01-03 16:34:25 -0500

    added assets

--- RobotArm/Assets/Samples/XR Interaction Toolkit/3.0.7/Starter Assets/Scripts/DestroySelf.cs
+++ RobotArm/Assets/Samples/XR Interaction Toolkit/3.0.7/Starter Assets/Scripts/DestroySelf.cs
@@ -1,29 +1,3 @@
-namespace UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets
-{
-    /// <summary>
-    /// Destroys the GameObject it is attached to after a specified amount of time.
-    /// </summary>
-    public class DestroySelf : MonoBehaviour
-    {
-        [SerializeField]
-        [Tooltip("The amount of time, in seconds, to wait after Start before destroying the GameObject.")]
-        float m_Lifetime = 0.25f;
-
-        /// <summary>
-        /// The amount of time, in seconds, to wait after Start before destroying the GameObject.
-        /// </summary>
-        public float lifetime
-        {
-            get => m_Lifetime;
-            set => m_Lifetime = value;
-        }
-
-        /// <summary>
-        /// See <see cref="MonoBehaviour"/>.
-        /// </summary>
-        void Start()
-        {
-            Destroy(gameObject, m_Lifetime);
-        }
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:733f6ed89c72e800c7c1400e370e4e6d3d28a9d1782e5258c9652ce86ffc9340
+size 856
