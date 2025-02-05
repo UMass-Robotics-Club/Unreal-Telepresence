d7073183148e7ae0f3df4117df40013f48e88e13
Matthew En <86386184+Mcen25@users.noreply.github.com>
2025-02-05 00:03:54 -0500

    Update ArmController.cs

--- RobotArm/Assets/Scripts/Ik/ArmController.cs
+++ RobotArm/Assets/Scripts/Ik/ArmController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8638df4c35c9af752e8d57aa312f549ec1259e45374444f08d90fec14a81c1f
-size 4114
+oid sha256:e2b0ee1086c5f79f3091fc2f4ea224f962fdafce8b98e8cf2ca30d509741957f
+size 3793
